c1f6d31c88621180d4c21f1cd69589916cbb54e9
adrien-laposta <adrien.la.posta@gmail.com>
2020-03-18 15:51:39 +0100

    reparation

--- correlation_foreground/corrmat_tools.py
+++ correlation_foreground/corrmat_tools.py
@@ -4,130 +4,151 @@ import numpy as np
 from toolbox import *
 import time
 
-def fisher(freq_list,fsky,names,cl_path):
+def compute_fisher(freq_list, fsky,names, cl_path):
+
     start_time = time.time()
-    deriv = [np.load(cl_path+"deriv_"+names[i]+".npy") for i in range(len(names))]
-    C_ell = np.load(cl_path+"CL.npy")
+
+    deriv = [np.load(cl_path + "deriv_" + names[i] + ".npy") for i in range(len(names))]
+    C_ell = np.load(cl_path + "CL.npy")
+
     print("Importation : %s secondes" % (time.time() - start_time))
-    lenght = len(deriv)
-    Nf = len(freq_list)
-    F = np.zeros((lenght,lenght))
+
+    n_params = len(deriv)
+    n_freqs = len(freq_list)
+    fisher = np.zeros((n_params, n_params))
+
     start_time = time.time()
-    for i in range(lenght):
-        for j in range(i+1):
-            ls = np.arange(len(C_ell)+2)[2:]
-            pf1 = fsky*(2*ls+1)/2
-            matprod = np.array([np.trace(np.linalg.inv(C_ell[a][:Nf,:Nf]).dot(deriv[i][a][:Nf,:Nf].dot(np.linalg.inv(C_ell[a][:Nf,:Nf]).dot(deriv[j][a][:Nf,:Nf])))) for a in range(len(C_ell))])
-            F[i,j] = np.sum(pf1*matprod)
-            F[j,i] = np.sum(pf1*matprod)
+
+    for ell in range(len(C_ell)):
+        trace_fisher = []
+        ls = np.arange(len(C_ell) + 2)[2:]
+        pre_fact = fsky * (2 * ls + 1)/2
+        inverse_c_ell = np.linalg.inv(C_ell[ell][:n_freqs,:n_freqs])
+        for i in range(n_params):
+            for j in range(n_params):
+                trace_mat_prod = np.trace(inverse_c_ell.dot(deriv[i][ell][:n_freqs, :n_freqs].dot(inverse_c_ell.dot(deriv[j][ell][:n_freqs, :n_freqs]))))
+                fisher[i, j] += pre_fact[ell] * trace_mat_prod
+
     print("Construction de Fisher : %s secondes" % (time.time() - start_time))
+
     #print(np.linalg.eigvals(F))
-    return(F)
+    return(fisher)
 
-def constraints(freq_list,fsky,names,cl_path):
-    F = fisher(freq_list,fsky,names,cl_path)
-    print(np.linalg.eigvals(F))
-    C = np.linalg.inv(F)
-    return(C)
+def constraints(freq_list, fsky,names, cl_path):
 
-def norm_fisher(freq_list,fsky,names,cl_path):
-    F = fisher(freq_list,fsky,names,cl_path)
-    norm_F = cov2corr(F,remove_diag=False)
-    eigen = np.linalg.eigvals(F)
+    fisher = compute_fisher(freq_list, fsky, names, cl_path)
+    print(np.linalg.eigvals(fisher))
+    cov = np.linalg.inv(fisher)
+
+    return(cov)
+
+def norm_fisher(freq_list, fsky, names, cl_path):
+
+    fisher = compute_fisher(freq_list, fsky, names, cl_path)
+    norm_fisher = cov2corr(fisher, remove_diag=False)
+    eigen = np.linalg.eigvals(fisher)
     #print("\n")
     #print("Normalized Fisher Matrix : ",norm_F)
     #print("\n")
     #print("Eigenvalues : ",eigen)
-    return(norm_F)
+    return(norm_fisher)
 
-def corrmat_evol(freq_list,name_param,save_path_fig,save_path_dat,fsky,names,cl_path):
-    fig = plt.figure(figsize=(24,13.5))
+def corrmat_evol(freq_list, name_param, save_path_fig, save_path_dat, fsky,names, cl_path):
+
+    fig = plt.figure(figsize=(24, 13.5))
     sig_mat = []
-    for i in range(1,len(freq_list)):
+    n_freqs = len(freq_list)
+    for i in range(1, n_freqs):
+
         print("\n")
-        print("-"*15)
-        print("Debut de la boucle a %s frequences" %(i+1))
+        print("-" * 15)
+        print("Debut de la boucle a %s frequences" %(i + 1))
+
         start_time_boucle = time.time()
-        covar = constraints(freq_list[:i+1],fsky,names,cl_path)
+        covar = constraints(freq_list[:i + 1], fsky, names, cl_path)
         sig = np.sqrt(np.diagonal(covar))
         sig_mat.append(sig)
-        np.savetxt(save_path_dat+str(freq_list[:i+1])+".dat",covar)
-        corr = cov2corr(covar,remove_diag=False)
-        ax = fig.add_subplot(231+i)
-        im = ax.imshow(corr,vmin=-1,vmax=+1,cmap='seismic')
+        np.savetxt(save_path_dat + str(freq_list[:i + 1]) + ".dat", covar)
+        corr = cov2corr(covar, remove_diag=False)
+        ax = fig.add_subplot(231 + i)
+        im = ax.imshow(corr, vmin= - 1, vmax= + 1, cmap='seismic')
         ax.set_xticks(np.arange(0, len(name_param), 1));
         ax.set_yticks(np.arange(0, len(name_param), 1));
         ax.set_xticklabels(name_param)
         ax.set_yticklabels(name_param)
-        ax.set_title(r'f = '+str(freq_list[:i+1]))
+        ax.set_title(r'f = ' + str(freq_list[:i + 1]))
+
         print("Fin de la boucle, temps d'execution : %s secondes" % (time.time() - start_time_boucle))
-        print("-"*15)
+        print("-" * 15)
+
     cb_ax = fig.add_axes([0.92, 0.1, 0.02, 0.8])
     cbar = fig.colorbar(im, cax=cb_ax)
     sig_mat = np.array(sig_mat)
-    np.savetxt(save_path_dat+"sigmas.dat",sig_mat)
-    fig.savefig(save_path_fig+"corrmat_var.png",dpi=300)
+    np.savetxt(save_path_dat + "sigmas.dat",sig_mat)
+    fig.savefig(save_path_fig + "corrmat_var.png", dpi=300)
 
-def fisher_norm_evol(freq_list,name_param,save_path_fig,fsky,names,cl_path):
-    for a in range(len(freq_list)):
-        fig = plt.figure(figsize=(24,24))
-        plt.rc('xtick',labelsize=20)
-        plt.rc('ytick',labelsize=20)
+def fisher_norm_evol(freq_list, name_param, save_path_fig, fsky, names, cl_path):
+
+    n_freqs = freq_list
+    for a in range(n_freqs):
+        fig = plt.figure(figsize=(24, 24))
+        plt.rc('xtick', labelsize=20)
+        plt.rc('ytick', labelsize=20)
         print("\n")
-        print("-"*15)
-        print("Debut de la boucle a %s frequences" %(a+1))
+        print("-" * 15)
+        print("Debut de la boucle a %s frequences" %(a + 1))
         start_time_boucle = time.time()
-        Fnorm = norm_fisher(freq_list[:a+1],fsky,names,cl_path)
+        Fnorm = norm_fisher(freq_list[:a + 1], fsky, names, cl_path)
         ax = fig.add_subplot(111)
-        im = ax.imshow(Fnorm,vmin=-1,vmax=+1,cmap='seismic')
+        im = ax.imshow(Fnorm, vmin= - 1, vmax= + 1, cmap='seismic')
         ax.set_xticks(np.arange(0, len(name_param), 1));
         ax.set_yticks(np.arange(0, len(name_param), 1));
         ax.set_xticklabels(name_param)
         ax.set_yticklabels(name_param)
-        ax.set_title(r'f = '+str(freq_list[:a+1]),fontsize=20)
+        ax.set_title(r'f = ' + str(freq_list[:a + 1]), fontsize=20)
         for i in range(len(Fnorm)):
             for j in range(len(Fnorm)):
-                ax.text(j, i, "{:0.3f}".format(Fnorm[i, j]),horizontalalignment="center",
+                ax.text(j, i, "{:0.3f}".format(Fnorm[i, j]), horizontalalignment="center",
                      color="black",fontsize=20)
         print("Fin de la boucle, temps d'execution : %s secondes" % (time.time() - start_time_boucle))
-        print("-"*15)
+        print("-" * 15)
         fig.tight_layout()
-        fig.savefig(save_path_fig+"fisher_norm_var"+str(a)+".png",dpi=300)
+        fig.savefig(save_path_fig + "fisher_norm_var" + str(a) + ".png",dpi=300)
 
-def cosmo_parameters(theta,fg_parameters,freq_list,name_param,save_path_fig,save_path_dat):
+def cosmo_parameters(theta, fg_parameters, freq_list, name_param, save_path_fig, save_path_dat):
 
-    fig = plt.figure(figsize=(24,13.5))
-    plt.rc('xtick',labelsize=14)
-    plt.rc('ytick',labelsize=14)
-    colors = ['darkred','darkorange','darkgreen','darkblue','darkviolet']
+    fig = plt.figure(figsize=(24, 13.5))
+    plt.rc('xtick', labelsize=14)
+    plt.rc('ytick', labelsize=14)
+    colors = ['darkred', 'darkorange', 'darkgreen', 'darkblue', 'darkviolet']
     for i in range(len(theta)):
-        sigma = np.loadtxt(save_path_dat+"sigmas.dat")[:,i]
-        ax = fig.add_subplot(231+i)
+        sigma = np.loadtxt(save_path_dat + "sigmas.dat")[:, i]
+        ax = fig.add_subplot(231 + i)
         ax.grid(True,linestyle='--')
-        abs = np.linspace(theta[i]-4*np.max(sigma),theta[i]+4*np.max(sigma),500)
+        abs = np.linspace(theta[i] - 4 * np.max(sigma), theta[i] + 4 * np.max(sigma),  500)
         for j in range(len(sigma)):
-            ord = gaussian(abs,theta[i],sigma[j])
-            ax.plot(abs,ord/np.max(ord),label=r'$N_{freq}$ = %s' %(j+2),color=colors[j])
-        ax.set_title(name_param[i],fontsize=20)
+            ord = gaussian(abs, theta[i], sigma[j])
+            ax.plot(abs, ord/np.max(ord), label=r'$N_{freq}$ = %s' %(j + 2),color=colors[j])
+        ax.set_title(name_param[i], fontsize=20)
         handles, labels = ax.get_legend_handles_labels()
-    fig.legend(handles, labels, loc='upper right',fontsize=18)
+    fig.legend(handles, labels, loc='upper right', fontsize=18)
     fig.tight_layout()
-    fig.savefig(save_path_fig+"forecast_var.png",dpi=300)
+    fig.savefig(save_path_fig + "forecast_var.png",dpi=300)
 
-    fig = plt.figure(figsize=(20,20))
-    plt.rc('xtick',labelsize=14)
-    plt.rc('ytick',labelsize=14)
-    colors = ['darkred','darkorange','darkgreen','darkblue','darkviolet']
+    fig = plt.figure(figsize=(20, 20))
+    plt.rc('xtick', labelsize=14)
+    plt.rc('ytick', labelsize=14)
+    colors = ['darkred', 'darkorange', 'darkgreen', 'darkblue', 'darkviolet']
     for i in range(len(fg_parameters)):
-        sigma = np.loadtxt(save_path_dat+"sigmas.dat")[:,i+6]
-        ax = fig.add_subplot(331+i)
-        ax.grid(True,linestyle='--')
-        abs = np.linspace(fg_parameters[i]-4*np.max(sigma),fg_parameters[i]+4*np.max(sigma),500)
+        sigma = np.loadtxt(save_path_dat + "sigmas.dat")[:, i + 6]
+        ax = fig.add_subplot(331 + i)
+        ax.grid(True, linestyle='--')
+        abs = np.linspace(fg_parameters[i] - 4 * np.max(sigma), fg_parameters[i] + 4 * np.max(sigma), 500)
         for j in range(len(sigma)):
-            ord = gaussian(abs,fg_parameters[i],sigma[j])
-            ax.plot(abs,ord/np.max(ord),label=r'$N_{freq}$ = %s' %(j+2),color=colors[j])
-        ax.set_title(name_param[i+6],fontsize=20)
+            ord = gaussian(abs, fg_parameters[i], sigma[j])
+            ax.plot(abs, ord/np.max(ord), label=r'$N_{freq}$ = %s' %(j + 2),color=colors[j])
+        ax.set_title(name_param[i + 6], fontsize=20)
         handles, labels = ax.get_legend_handles_labels()
-    fig.legend(handles, labels, loc=(0.6,0.1),fontsize=24)
+    fig.legend(handles, labels, loc=(0.6,0.1), fontsize=24)
     fig.tight_layout()
-    fig.savefig(save_path_fig+"forecast_var_fg.png",dpi=300)
+    fig.savefig(save_path_fig + "forecast_var_fg.png",dpi=300)
--- correlation_foreground/pre_calc_corr.py
+++ correlation_foreground/pre_calc_corr.py
@@ -1,104 +1,106 @@
-import sys, platform, os
+import sys,  platform,  os
 import matplotlib.pyplot as plt
 import numpy as np
 import camb
-from camb import model,initialpower
+from camb import model, initialpower
 from toolbox import *
 from pspy import pspy_utils, so_dict
 import mflike as mfl
 
-def noise_freq(freq_list,noise_data_path,ell_max):
+def noise_freq(freq_list, noise_data_path, ell_max):
     noise_list_diag = []
-
-    for element in freq_list:
-        noise_t_spectrum = np.loadtxt(noise_data_path+"noise_t_LAT_"+str(element)+"xLAT_"+str(element)+".dat")[:,1][:ell_max-1]
+    n_freqs = len(freq_list)
+    for freq in freq_list:
+        noise_t_spectrum = np.loadtxt(noise_data_path+"noise_t_LAT_"+str(freq)+"xLAT_"+str(freq)+".dat")[:, 1][:ell_max-1]
         noise_list_diag.append(noise_t_spectrum)
 
     noise_list_matrix = []
 
-    for a in range(ell_max-1):
-        noise_matrix = np.zeros((len(freq_list),len(freq_list)))
-        for i in range(len(freq_list)):
-            noise_matrix[i,i] = noise_list_diag[i][a]
+    for ell in range(ell_max-1):
+        noise_matrix = np.zeros((n_freqs, n_freqs))
+        for i in range(n_freqs):
+            noise_matrix[i, i] = noise_list_diag[i][ell]
         noise_list_matrix.append(noise_matrix)
 
     return(np.array(noise_list_matrix))
 
-def CL(theta,fg_parameters,ell_max,freq_list,noise_list_matrix,mnu,omk,r):
+def get_cl_array(theta, fg_parameters, ell_max, freq_list, noise_list_matrix):
 
     d = so_dict.so_dict()
     d.read_from_file("global_healpix_example.dict")
     fg_norm = d["fg_norm"]
-    components = {"tt": d["fg_components"],"ee": [],"te": []}
-    fg_model = {"normalisation": fg_norm,"components": components}
-    fg_params = {'a_tSZ': fg_parameters[0],'a_kSZ': fg_parameters[1],'a_p': fg_parameters[2],'beta_p': fg_parameters[3],
-                    'a_c': fg_parameters[4],'beta_c': fg_parameters[5],'n_CIBC': 1.2,'a_s': fg_parameters[6],
+    components = {"tt": d["fg_components"], "ee": [], "te": []}
+    fg_model = {"normalisation": fg_norm, "components": components}
+    fg_params = {'a_tSZ': fg_parameters[0], 'a_kSZ': fg_parameters[1], 'a_p': fg_parameters[2], 'beta_p': fg_parameters[3],
+                    'a_c': fg_parameters[4], 'beta_c': fg_parameters[5], 'n_CIBC': 1.2, 'a_s': fg_parameters[6],
                     'T_d': 9.6}
 
     pars = camb.CAMBparams()
-    pars.set_cosmology(H0=theta[0],ombh2=theta[1],omch2=theta[2],mnu=mnu,omk=omk,tau=theta[5])
-    pars.InitPower.set_params(As=theta[3],ns=theta[4],r=r)
-    pars.set_for_lmax(ell_max-1,lens_potential_accuracy=0)
+    pars.set_cosmology(H0=theta[0], ombh2=theta[1], omch2=theta[2], mnu=0.06, omk=0, tau=theta[5])
+    pars.InitPower.set_params(As=theta[3], ns=theta[4], r=0)
+    pars.set_for_lmax(ell_max-1, lens_potential_accuracy=0)
     results = camb.get_results(pars)
-    powers = results.get_cmb_power_spectra(pars,CMB_unit="muK")
+    powers = results.get_cmb_power_spectra(pars, CMB_unit="muK")
     totCL=powers['total']
 
-    TT = totCL[:,0][2:]
+    TT = totCL[:, 0][2:]
     ell_list = np.arange(len(TT)+2)[2:]
 
     power_spectrum_matrix = []
 
-    noise_list_matrix = [element[:len(freq_list),:len(freq_list)] for element in noise_list_matrix]
+    noise_list_matrix = [element[:len(freq_list), :len(freq_list)] for element in noise_list_matrix]
 
-    fg_dict = mfl.get_foreground_model(fg_params,fg_model,freq_list,ell_list)
+    fg_dict = mfl.get_foreground_model(fg_params, fg_model, freq_list, ell_list)
 
-    for a in range(len(ell_list)):
-        c_matrix = np.ones((len(freq_list),len(freq_list)))*TT[a]
-        for i in range(len(freq_list)):
-            for j in range(i+1):
-                c_matrix[i,j] = c_matrix[i,j] + noise_list_matrix[a][i,j] + fg_dict['tt','all',freq_list[i],freq_list[j]][a]
-                c_matrix[j,i] = c_matrix[j,i] + noise_list_matrix[a][j,i] + fg_dict['tt','all',freq_list[j],freq_list[i]][a]
+    n_ell = len(ell_list)
+    n_freqs = len(freq_list)
+
+    for ell in range(n_ell):
+        c_matrix = np.ones((n_freqs, n_freqs))*TT[ell]
+        for i in range(n_freqs):
+            for j in range(n_freqs):
+                c_matrix[i, j] = c_matrix[i, j] + noise_list_matrix[ell][i, j] + fg_dict['tt', 'all', freq_list[i], freq_list[j]][ell]
         power_spectrum_matrix.append(c_matrix)
 
     return(np.array(power_spectrum_matrix))
 
-def dCL(theta,fg_parameters,ell_max,freq_list,noise_list_matrix,mnu,omk,r):
+def get_cl_derivatives(theta, fg_parameters, ell_max, freq_list, noise_list_matrix):
 
-    length_cosmo = len(theta)
-    length_fg = len(fg_parameters)
+    n_params_cosmo = len(theta)
+    n_params_fg = len(fg_parameters)
     epsilon_cosmo = theta/100
     epsilon_fg = fg_parameters/100
     var_temp = []
 
-    for i in range(length_cosmo):
-        eps = epsilon_cosmo[i]*np.eye(1,length_cosmo,i)
+    for i in range(n_params_cosmo):
+        eps = epsilon_cosmo[i]*np.eye(1, n_params_cosmo, i)
         eps = eps.flatten()
-        CL_plus = CL(theta+eps,fg_parameters,ell_max,freq_list,noise_list_matrix,mnu,omk,r)
-        CL_moins = CL(theta-eps,fg_parameters,ell_max,freq_list,noise_list_matrix,mnu,omk,r)
+        CL_plus = get_cl_array(theta+eps, fg_parameters, ell_max, freq_list, noise_list_matrix)
+        CL_moins = get_cl_array(theta-eps, fg_parameters, ell_max, freq_list, noise_list_matrix)
         der = (CL_plus-CL_moins)/(2*epsilon_cosmo[i])
         var_temp.append(der)
 
-    for j in range(length_fg):
-        eps = epsilon_fg[j]*np.eye(1,length_fg,j)
+    for j in range(n_params_fg):
+        eps = epsilon_fg[j]*np.eye(1, n_params_fg, j)
         eps = eps.flatten()
-        CL_plus = CL(theta,fg_parameters+eps,ell_max,freq_list,noise_list_matrix,mnu,omk,r)
-        CL_moins = CL(theta,fg_parameters-eps,ell_max,freq_list,noise_list_matrix,mnu,omk,r)
+        CL_plus = get_cl_array(theta, fg_parameters+eps, ell_max, freq_list, noise_list_matrix)
+        CL_moins = get_cl_array(theta, fg_parameters-eps, ell_max, freq_list, noise_list_matrix)
         der = (CL_plus-CL_moins)/(2*epsilon_fg[j])
         var_temp.append(der)
 
     return(var_temp)
 
-def pre_calculation(theta,fg_parameters,ell_max,freq_list,noise_data_path,mnu,omk,r,save_path,names):
+def pre_calculation(theta, fg_parameters, ell_max, freq_list, noise_data_path, save_path, names):
 
     if not os.path.isdir(save_path):
         os.mkdir(save_path)
 
-    noise_list_matrix = noise_freq(freq_list,noise_data_path,ell_max)
-    np.save(save_path+'noise',noise_list_matrix)
+    noise_list_matrix = noise_freq(freq_list, noise_data_path, ell_max)
+    np.save(save_path+'noise', noise_list_matrix)
 
-    power_spectrums = CL(theta,fg_parameters,ell_max,freq_list,noise_list_matrix,mnu,omk,r)
-    np.save(save_path+'CL',power_spectrums)
+    power_spectrums = get_cl_array(theta, fg_parameters, ell_max, freq_list, noise_list_matrix)
+    np.save(save_path+'CL', power_spectrums)
 
-    deriv = dCL(theta,fg_parameters,ell_max,freq_list,noise_list_matrix,mnu,omk,r)
+    deriv = get_cl_derivatives(theta, fg_parameters, ell_max, freq_list, noise_list_matrix)
     for i in range(len(names)):
-        np.save(save_path+'deriv_'+names[i],deriv[i])
+        np.save(save_path+'deriv_'+names[i], deriv[i])
