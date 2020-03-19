22d93e66e64438e53c0340058e96193e000cedc7
Xavier Garrido <xavier.garrido@gmail.com>
2020-03-19 14:25:34 +0100

    use numpy everywhere even on scalar

--- correlation_foreground/toolbox.py
+++ correlation_foreground/toolbox.py
@@ -1,25 +1,28 @@
 import numpy as np
-from math import pi, cos, sin
-import sys, platform, os
 
-def cov2corr(cov,remove_diag=True):
-    d= np.sqrt(cov.diagonal())
-    corr = ((cov.T/d).T)/d
+
+def cov2corr(cov, remove_diag=True):
+    d = np.sqrt(cov.diagonal())
+    corr = ((cov.T / d).T) / d
     if remove_diag:
         corr -= np.identity(cov.shape[0])
-    return(corr)
+    return corr
+
 
 def gaussian(x, mu, sig):
-    return np.exp(-np.power(x - mu, 2.) / (2 * np.power(sig, 2.)))/(np.sqrt(2*np.pi*sig*sig))
+    return np.exp(-np.power(x - mu, 2.) /
+                  (2 * np.power(sig, 2.))) / (np.sqrt(2 * np.pi * sig * sig))
 
-def Ellipse(center,a,b,t_rot):
-    u,v = center
-    t = np.linspace(0, 2*pi, 100)
-    Ell = np.array([a*np.cos(t) , b*np.sin(t)])
+
+def Ellipse(center, a, b, t_rot):
+    u, v = center
+    t = np.linspace(0, 2 * np.pi, 100)
+    Ell = np.array([a * np.cos(t), b * np.sin(t)])
     #u,v removed to keep the same center location
-    R_rot = np.array([[cos(t_rot) , -sin(t_rot)],[sin(t_rot) , cos(t_rot)]])
+    R_rot = np.array([[np.cos(t_rot), -np.sin(t_rot)],
+                      [np.sin(t_rot), np.cos(t_rot)]])
     #2-D rotation matrix
-    Ell_rot = np.zeros((2,Ell.shape[1]))
+    Ell_rot = np.zeros((2, Ell.shape[1]))
     for i in range(Ell.shape[1]):
-        Ell_rot[:,i] = np.dot(R_rot,Ell[:,i])
-    return(u+Ell_rot[0,:],v+Ell_rot[1,:])
+        Ell_rot[:, i] = np.dot(R_rot, Ell[:, i])
+    return u + Ell_rot[0, :], v + Ell_rot[1, :]
